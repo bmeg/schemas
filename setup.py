69ad5f3549676af82bca7c753a00e350d6a9b5f7
Ryan Spangler <ryan.spangler@gmail.com>
2018-01-30 14:08:58 -0800

    README

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 
 setup(
     name='biostream-schemas',
-    version='0.0.1',
-    scripts=[
-        'bmeg/clinical_pb2.py',
-        'bmeg/cna_pb2.py',
-        'bmeg/genome_pb2.py',
-        'bmeg/phenotype_pb2.py',
-        'bmeg/rna_pb2.py',
-        'bmeg/variants_pb2.py'
-    ]
+    version='0.0.2',
+    packages=['bmeg']
 )
+
+# [
+#         'bmeg/clinical_pb2.py',
+#         'bmeg/cna_pb2.py',
+#         'bmeg/genome_pb2.py',
+#         'bmeg/phenotype_pb2.py',
+#         'bmeg/rna_pb2.py',
+#         'bmeg/variants_pb2.py'
+#     ]
